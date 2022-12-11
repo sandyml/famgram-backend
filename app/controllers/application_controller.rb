03a222791afe363411dbf3f16da511171bd66c75
sandym <91996681+sandyml@users.noreply.github.com>
2022-12-11 13:27:27 -0500

    Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  # this is a GET request  
+  # example 
+#   get "/" do
+#     answer = 12345 + 7
+#     { message: "Goodluck with your project and also the answer is #{ answer } }.to_json
+#   end
+# end
+
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  
+  # get "/hello-world" do
+  #   { something_else: "Hello World!" }
+  # end
+  
+  get "/photos" do
+    { message: "This is Gram Photos" }.to_json
+  end
+  # get "/gram-photos" do
+  #   photos = Photo.all
+  #   photos.to_json
+  # end
 
 end
