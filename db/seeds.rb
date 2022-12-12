23d8c6d589dfa1961da00ff13ac02e890a67fda9
sandym <91996681+sandyml@users.noreply.github.com>
2022-12-11 19:03:41 -0500

    config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ puts "🌱 Seeding spices..."
 Quote.create(title: "Quote 1", description: "Your direction is more important than your speed.")
 Quote.create(title: "Quote 2", description: "If they dont know you personally. Dont take it personal.")
 
-puts "✅ ALL Done seeding!"
+puts "✅🌱 Done seeding! Ready to plant"
