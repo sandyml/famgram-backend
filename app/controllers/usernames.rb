7dd66aa19ba7e0cf2435752b472c4ce7b23b0992
sandym <91996681+sandyml@users.noreply.github.com>
2022-12-30 09:33:01 -0500

    Updated

--- app/controllers/usernames.rb
+++ app/controllers/usernames.rb
@@ -9,7 +9,29 @@ class UsernamesController < ApplicationController
   user = User.find(params[:id])
   user.to_json(include: [:quotes])
   end
+
+  # render all users/blogger as JSON from backend 
+  get "/users" do 
+    Username.all.to_json(include: [quotes: {only: [:id, ]}])
+  end 
  
+  get "/users/:id" do 
+    user = Username.find_by_id(params["id"])
+    user.to_json(include: [:quotes])
+  end 
+
+  # POST usernames then return obj -> json if it is saved using if/else if not throw error 
+  post "users" do
+    user = Username.new(params)
+    if user.save 
+      user.to_json(include: [:quotes])
+    else { errors: user.errors.full_messages }.to_json
+    end 
+  end 
+
+  # patch user to :id
+  patch "quotes "
+
 #  get "/usernames" do
 #   usernames = Quote.all 
 #   usernames.to_json 
